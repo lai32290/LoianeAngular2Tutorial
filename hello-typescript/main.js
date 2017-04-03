3208a72ee770c5746040481cea401978094b49bb
Lai <lai32690@gmail.com>
2017-04-03 17:34:02 -0300

    first commit

---
+++ hello-typescript/main.js
@@ -0,0 +1,14 @@
+var variable = 'my variable';
+function myFunc(x, y) {
+    return x + y;
+}
+var num = 3;
+var Matematica = (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'blabla';
---
+++ hello-typescript/main.ts
@@ -0,0 +1,15 @@
+var variable = 'my variable';
+
+function myFunc(x, y) {
+    return x + y;
+}
+
+let num = 3;
+
+class Matematica {
+    soma(x, y) {
+        return x + y;
+    }
+}
+
+var n1: string = 'blabla';
