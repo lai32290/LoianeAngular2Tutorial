11f31ff2863d427c221c85e6a6066ebc48269dba
Lai <lai32690@gmail.com>
2017-04-05 23:39:34 -0300

    add data-binding example

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url = 'http://blabla.com';
+  urlImage = 'http://lorempixel.com/400/200/nature';
+  cursoAngular = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso(){ return true; }
+
+}
