4364065baa219c39e983ac703aae031bc4a66f01
Lai <lai32690@gmail.com>
2017-04-06 22:35:15 -0300

    add event binding exampe

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
+  styles: [
+      `.highlight {
+          background-color: yellow;
+          font-weight: bold;
+      }`
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   url = 'http://blabla.com';
   urlImage = 'http://lorempixel.com/400/200/nature';
   cursoAngular = true;
+  valorAtual = '';
+  valorSalvo = '';
+  isMouseOver = false;
 
   constructor() { }
 
@@ -21,5 +30,26 @@ export class DataBindingComponent implements OnInit {
   }
 
   getCurtirCurso(){ return true; }
+
+  bottaoClicado() {
+    alert('click');
+  }
+
+  onKeyUp(e: KeyboardEvent) {
+    console.log((<HTMLInputElement>e.target).value);
+    this.valorAtual = (<HTMLInputElement>e.target).value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOver(){
+    this.isMouseOver = true;
+  }
+
+  onMouseOut(){
+    this.isMouseOver = false;
+  }
 
 }
